cea862aa462c42bcb3007267e220f57ffc1fbcf9
diana <diana.v.danilenko@gmail.com>
2023-05-29 16:53:56 +0200

    29.05 update
    uploaded graphs and cleaned some code

--- 260423_graphs.R
+++ 260423_graphs.R
@@ -5,52 +5,7 @@ library("dplyr")
 library("scales")
 library("leaflet")
 
-df1 <- data.frame(
-  category = c("male", "female"),
-  count = c(22955, 14527),
-  percents = c(100*22955/(22955+14527),100*14527/(22955+14527))
-)
-df2 <- data.frame(
-  category = c("male", "female"),
-  count = c(26764, 11326),
-  percents = c(100*26764/(26764+11326),100*11326/(26764+11326))
-)
-df3 <- data.frame(
-  category = c("male", "female"),
-  count = c(19837, 6039),
-  percents = c(100*19837/(19837+6039),100*6039/(19837+6039))
-)
-
-# make a pie chart
-ggplot(df1, aes(x="", y=count, fill=category)) +
-  geom_bar(stat="identity", width=1) +
-  coord_polar("y", start=0) +
-  theme_void() +
-  labs(title = "Predicted gender of the first author") +
-  geom_label(aes(label = paste(percents,"%")), size = 4,
-             fill = "white", position = position_stack(vjust = 0.5)) +
-  scale_fill_manual(values = c(wes_palette("Darjeeling1")[4], wes_palette("Darjeeling1")[2])) +
-  theme(aspect.ratio=1,
-        plot.title = element_text(hjust = 0.5, size = 14),
-        plot.subtitle = element_text(hjust = 0.5))
-
-# combine the three data frames
-df_all <- rbind(df1, df2, df3)
-df_all$percents <- round(df_all$percents, 2)
-df_all$position <- c("first author", "first author", "last author", "last author", "majority author", "majority author")
-
-# create the histogram
-ggplot(data = df_all, aes(fill=category, y=percents, x=position)) + 
-  geom_bar(position="stack", stat="identity") +
-  geom_label(aes(label = paste(percents,"%")), size = 4,
-             fill = "white", position = position_stack(vjust = 0.5)) +
-  scale_fill_manual(values = c(wes_palette("Darjeeling1")[4], wes_palette("Darjeeling1")[2])) +
-  xlab("") + ylab("percentage")+
-  theme(aspect.ratio=1,
-        plot.title = element_text(hjust = 0.5, size = 14),
-        plot.subtitle = element_text(hjust = 0.5)) +
-  labs(title = "Composition of authors by gender",
-  subtitle="in authorship instances with an unambiguous gender estimate")
+### DESCRPITIVE STATISTICS ###
 
 # make a graph for cumulative yearly publications             
 pub_y <- read_csv("yearly_cumulative_papers.csv")
@@ -64,9 +19,6 @@ ggplot(pub_y, aes(x = year, y = cumulative_sum)) +
   theme(aspect.ratio=0.75,
         plot.title = element_text(hjust = 0.5, size = 14),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) 
-
-# make country graphs for first author country and last author country
-# most comprehensive datasets were chosen as representative to avoid repetition of the visualisations
 
 first_country <- subset(data1, select = c(id,country, gender.inequality.index,gii_quartile))
 first_country <- filter(first_country,country != "")
@@ -125,3 +77,39 @@ journal <- merge(journal, quartile_values, by = "journal")
 write.csv(impact_count, "impact_count.csv", row.names = FALSE)
 write.csv(gii_count1, "gii_count1.csv", row.names = FALSE)
 write.csv(gii_count3, "gii_count3.csv", row.names = FALSE)
+
+### RESULTS - GENDER COMPOSITION ###
+df1 <- data.frame(
+  category = c("male", "female"),
+  count = c(22955, 14527),
+  percents = c(100*22955/(22955+14527),100*14527/(22955+14527))
+)
+df2 <- data.frame(
+  category = c("male", "female"),
+  count = c(26764, 11326),
+  percents = c(100*26764/(26764+11326),100*11326/(26764+11326))
+)
+df3 <- data.frame(
+  category = c("male", "female"),
+  count = c(19837, 6039),
+  percents = c(100*19837/(19837+6039),100*6039/(19837+6039))
+)
+
+# combine the three data frames
+df_all <- rbind(df1, df2, df3)
+df_all$percents <- round(df_all$percents, 2)
+df_all$position <- c("first author", "first author", "last author", "last author", "majority author", "majority author")
+
+# create the histogram
+ggplot(data = df_all, aes(fill=category, y=percents, x=position)) + 
+  geom_bar(position="stack", stat="identity") +
+  geom_label(aes(label = paste(percents,"%")), size = 4,
+             fill = "white", position = position_stack(vjust = 0.5)) +
+  scale_fill_manual(values = c(wes_palette("Darjeeling1")[4], wes_palette("Darjeeling1")[2])) +
+  xlab("") + ylab("percentage")+
+  theme(aspect.ratio=1,
+        plot.title = element_text(hjust = 0.5, size = 14),
+        plot.subtitle = element_text(hjust = 0.5)) +
+  labs(title = "Composition of authors by gender",
+       subtitle="in authorship instances with an unambiguous gender estimate")
+
